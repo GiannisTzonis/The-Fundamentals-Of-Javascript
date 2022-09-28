bda921006f586821d9f1943bfb2c428641d5934e
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-28 17:51:57 +0300

    fifth

---
+++ scriptFive.js
@@ -0,0 +1,11 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+function booleanToString(b){
+    if(b){
+    return "true";
+  }else{
+    return "false";
+  }
+}
