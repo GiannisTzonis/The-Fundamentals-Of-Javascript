7ce855ed5a6f286b47f7ee3825247ec8f9a5e8c8
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-28 18:33:10 +0300

    seventh

---
+++ scriptSeven.js
@@ -0,0 +1,18 @@
+/*
+In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+*/
+
+function nbYear(p0, percent, aug, p) {
+    let years = 0;
+    for (years; p0 < p; years++) {
+      p0 += p0 * (percent / 100) + aug;
+    }
+    return years;
+  }
