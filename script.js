0dfdeda40e50d3740baef894c831b3ff4c685978
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-28 14:22:21 +0300

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+/*Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11*/
+
+function basicOp(operation, value1, value2) {
+    if (operation === '+') {
+      output = value1 + value2
+    } else if (operation ==='-') {
+      output = value1 - value2
+    } else if (operation === '*') {
+      output = value1 * value2
+    } else if (operation === '/') {
+      output = value1 / value2
+    }
+    return output
+  }
