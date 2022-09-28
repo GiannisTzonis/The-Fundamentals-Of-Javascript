629538e4dcc2b1941c8883a604051a634e357838
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-28 18:46:21 +0300

    eigth

--- scriptSeven.js
+++ scriptSeven.js
@@ -9,10 +9,14 @@ aug is an integer, percent a positive or null floating number, p0 and p are posi
 
 */
 
-function nbYear(p0, percent, aug, p) {
-    let years = 0;
-    for (years; p0 < p; years++) {
-      p0 += p0 * (percent / 100) + aug;
-    }
-    return years;
+function nbYear(p0, percent, aug, p, years = 0) {
+    return p0 >= p
+      ? years
+      : nbYear(
+          Math.floor(p0 + p0 * (percent / 100)) + aug,
+          percent,
+          aug,
+          p,
+          years + 1
+        );
   }
