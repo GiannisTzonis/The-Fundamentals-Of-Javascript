96977531f974b8d26a301b77a3ed3fdc2c2ff8cf
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-01 00:17:16 +0300

    17

---
+++ scriptSeventeen.js
@@ -0,0 +1,12 @@
+/*
+
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+
+*/
+
+function doubleChar(str) {
+    return [...str].map(s => s.repeat(2)).join('')
+  }
