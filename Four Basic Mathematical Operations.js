17bba12ed7a16c4fd36f5a3657e5ceea6ea0a9b1
Giannis Tzonis <40187515+GiannisTzonis@users.noreply.github.com>
2022-10-01 15:01:15 +0300

    Update Four Basic Mathematical Operations.js

--- Four Basic Mathematical Operations.js
+++ Four Basic Mathematical Operations.js
@@ -1,4 +1,4 @@
-/*Your task is to create a function that does four basic mathematical operations.
+/*Your task is to create a function that does four basic mathematical operations. 
 
 The function should take three arguments - operation(string/char), value1(number), value2(number).
 The function should return result of numbers after applying the chosen operation.
