c1c3c8aed0cacf1cd077b747656ca7707fa634e9
Giannis Tzonis <40187515+GiannisTzonis@users.noreply.github.com>
2022-10-01 21:28:25 +0300

    32

--- switchNumbersToWords.js
+++ switchNumbersToWords.js
@@ -9,47 +9,25 @@ Output :: "One".
 */
 
 function switchItUp(number) {
-  2;
   if (number === 1) {
-    3;
     return "One";
-    4;
   } else if (number === 2) {
-    5;
     return "Two";
-    6;
   } else if (number === 3) {
-    7;
     return "Three";
-    8;
   } else if (number === 4) {
-    9;
     return "Four";
-    10;
   } else if (number === 5) {
-    11;
     return "Five";
-    12;
   } else if (number === 6) {
-    13;
     return "Six";
-    14;
   } else if (number === 7) {
-    15;
     return "Seven";
-    16;
   } else if (number === 8) {
-    17;
     return "Eight";
-    18;
   } else if (number === 9) {
-    19;
     return "Nine";
-    20;
   } else if (number === 0) {
-    21;
     return "Zero";
-    22;
   }
-  23;
 }
