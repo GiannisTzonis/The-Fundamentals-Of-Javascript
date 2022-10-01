157dd1df64b9cd470e6f872ca8cdf87bbca33f0d
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-01 20:30:01 +0300

    27

---
+++ sortOddNumbersInOrder.js
@@ -0,0 +1,15 @@
+/*
+
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+*/
+
+function sortArray(array) {
+    let indices = [];
+  
+    array
+        .filter((v, i) => v % 2 && indices.push(i))
+        .sort((a, b) => a - b)
+        .forEach((v, i) => array[indices[i]] = v);
+    return array
+}
