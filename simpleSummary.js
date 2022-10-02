73bdc730e13d22966c651b97f5cb2b25b60a05f5
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-02 15:30:15 +0300

    37

---
+++ simpleSummary.js
@@ -0,0 +1,18 @@
+/*
+
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+*/
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  let goalSummary = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return goalSummary;
+}
