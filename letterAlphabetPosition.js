4a4006849928621689582976885b838b6219edcc
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-03 23:45:26 +0300

    46

---
+++ letterAlphabetPosition.js
@@ -0,0 +1,54 @@
+/*
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string 
+
+*/
+
+function alphabetPosition(text) {
+  var result = [];
+  var alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  text = text
+    .replace(/\W*\d+/g, "")
+    .toLowerCase()
+    .split("");
+  for (var i = 0; i < text.length; i++) {
+    var j = alphabet.indexOf(text[i]) + 1;
+    if (j) result.push(j);
+  }
+  return result.join(" ");
+}
