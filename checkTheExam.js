94f78b2b62867c6bd47f5b4cf01c09b3e023f5ef
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-09 21:49:44 +0300

    63

---
+++ checkTheExam.js
@@ -0,0 +1,41 @@
+/*
+
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+*/
+
+function checkExam(array1, array2) {
+ 			
+    let correct = 0;
+    let error = 0;
+    let other = 0;
+               
+    for(let i=0;i<array1.length;i++){
+            if(array1[i] == array2[i]) {
+                correct ++;
+            } else if (array2[i] == '') {
+                other = 0;
+            } else {
+                error ++;
+            }
+    }
+    
+    let result = correct * 4 + error * (-1) + other;
+           
+    if ( result < 0 ) {
+             return 0;
+           }
+    
+    return result;
+}
